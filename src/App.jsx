52417fe7c893cf69de609770945e3688f0085efc
Alexey Nizovtsev <Niovtsev.alexey@gmail.com>
2025-07-11 15:34:20 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
